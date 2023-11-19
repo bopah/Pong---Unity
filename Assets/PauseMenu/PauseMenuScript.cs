16c34a87b874b6fcc59fa42e49c0c1d1f4502946
bopah <bopah91@hotmail.com>
2023-11-19 10:46:17 +0100

    added pause screen, with resume + restart buttons

---
+++ Assets/PauseMenu/PauseMenuScript.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; // This is necessary for loading and unloading scenes
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        this.Resume();
+    }
+}
